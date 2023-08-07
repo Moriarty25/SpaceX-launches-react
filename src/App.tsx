7adcfbb8c7373a157b202eb270e34afde5b6cc5a
Moriarty25 <afanasev.andrey99@gmail.com>
2023-08-07 12:48:42 +0300

    refactor: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,26 @@ import { launchesApi, useGetLaunchesQuery } from './store';
 import { Button, Dropdown, Space, Spin, Typography, message } from 'antd';
 import { SortAscendingOutlined, SortDescendingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { useAppDispatch } from './hooks/redux';
 import { useScroll } from './hooks/useScroll';
 import { Launch } from './components/Launch';
 import { ILaunch } from './models/launchTypes';
 
 export const App = () => {
-	const [launch, setLaunch] = useState(false);
 	const [page, setPage] = useState(0);
 	const [sort, setSort] = useState<'ascending' | 'descending'>('descending');
-	const { data, currentData, isLoading, isFetching } = useGetLaunchesQuery({ page, sort });
+	const { data, isLoading } = useGetLaunchesQuery({ page, sort });
 	const parentRef = useRef(null);
 	const childRef = useRef(null);
 	const dispatch = useAppDispatch();
-
-	const launchesHandler = () => {
-		//if (!data?.nextPage) return
-
-		setPage(prev => prev + 1);
-
-		// setLaunch(prev => [...prev, data])
-	};
-
 	const intersected = useScroll(parentRef, childRef, () => launchesHandler());
+
+	function launchesHandler() {
+		setPage(prev => prev + 1);
+	}
 
 	useEffect(() => {
 		if (data) {
-			if (data.nextPage === null) setLaunch(true);
 			// const s = data.docs.filter((launch: { success: boolean; }) => launch.success)
 			const s =
 				new Date(data?.docs[length]?.date_utc).getUTCFullYear() -
@@ -79,28 +72,26 @@ export const App = () => {
 	};
 
 	return (
-		<>
-			<div className='app'>
-				<header className='app__header'>
-					<Typography.Title level={3}>
-						Successful SpaceX launches for 2015-2019
-					</Typography.Title>
-					<div className='sort'>
-						<Typography.Text style={{ fontSize: 16 }}>Sort by time:</Typography.Text>
-						<Dropdown menu={dropdownMenu}>
-							<Button>
-								<Space>{sort}</Space>
-							</Button>
-						</Dropdown>
-					</div>
-				</header>
-				<main className='launches' ref={parentRef}>
-					{isLoading ? <Spin size='large' /> : launchesElements}
-					<div ref={childRef} style={{ height: 50, color: 'black' }}>
-						...
-					</div>
-				</main>
-			</div>
-		</>
+		<div className='app'>
+			<header className='app__header'>
+				<Typography.Title level={3}>
+					Successful SpaceX launches for 2015-2019
+				</Typography.Title>
+				<div className='sort'>
+					<Typography.Text style={{ fontSize: 16 }}>Sort by time:</Typography.Text>
+					<Dropdown menu={dropdownMenu}>
+						<Button>
+							<Space>{sort}</Space>
+						</Button>
+					</Dropdown>
+				</div>
+			</header>
+			<main className='launches' ref={parentRef}>
+				{isLoading ? <Spin size='large' /> : launchesElements}
+				<div ref={childRef} style={{ height: 50, color: 'black' }}>
+					...
+				</div>
+			</main>
+		</div>
 	);
 };
