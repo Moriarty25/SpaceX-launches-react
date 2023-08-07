74fd66cbfd56b5a26bafef7ffbfa589d14ad8d91
Moriarty25 <afanasev.andrey99@gmail.com>
2023-08-08 02:57:28 +0300

    feat: added test case on Launch

--- src/tests/Launch.test.tsx
+++ src/tests/Launch.test.tsx
@@ -1,22 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { renderWithProviders } from '../utils/test-utils';
 import { Launch } from '../components/Launch';
 
-test('Launch component is rendering ', () => {
-    const mockLaunchProps = {
-        name: 'SpaceX',
-        date: new Date(),
-        rocket: 'id',
-    };
-    render(
-        <Launch
-        date={mockLaunchProps.date}
-            name={mockLaunchProps.name}
-            rocket={mockLaunchProps.rocket}
-            details={null}
-        />,
-    );
-    const details = screen.getByText(/'No details about this mission'/i);
-    expect(details).toBeNull();
-    
+const mockLaunchProps = {
+    name: 'CRS-11',
+    date: new Date('2019-12-17T00:10:00.000Z'),
+    rocket: '123',
+};
+
+describe('Launch component ', () => {
+    it('check component is rendering', async () => {
+        renderWithProviders(
+            <Launch
+                date={mockLaunchProps.date}
+                name={mockLaunchProps.name}
+                rocket={mockLaunchProps.rocket}
+                details={null}
+            />,
+        )
+
+        // Rendering name of mission
+        expect(await screen.getByText('CRS-11')).toBeInTheDocument();
+
+        // Rendering message if isn't details
+        expect(screen.getByText('No details about this mission')).toBeInTheDocument();
+  
+        // Rendering correct Date 
+        expect(screen.getByText('Tue, 17 Dec 2019 00:10:00 GMT')).toBeInTheDocument();
+    });
+
 });
---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,35 @@
+import { launchesApi } from '../store/launchesApi';
+import React, { PropsWithChildren } from 'react'
+import { render } from '@testing-library/react'
+import type { RenderOptions } from '@testing-library/react'
+import { configureStore } from '@reduxjs/toolkit'
+import type { PreloadedState } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+import type { RootState } from '../store'
+
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: any
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    store = configureStore({
+      reducer: {
+        [launchesApi.reducerPath]: launchesApi.reducer,
+      },
+      middleware: getDefaultMiddleware => getDefaultMiddleware().concat(launchesApi.middleware),
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  const Wrapper = ({ children }: PropsWithChildren<{}>): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
